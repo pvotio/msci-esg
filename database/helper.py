c6f358b3573e5add3050b2302917eec38a78e293
Ali Moghimi <mghmiali@gmail.com>
2024-11-30 08:45:15 +0000

    feat: mssql ad login

--- database/helper.py
+++ database/helper.py
@@ -1,36 +1,13 @@
-from sqlalchemy import text
-
 from config import settings
-from database import MSSQLDatabaseConnection, PandasSQLDataInserter
+from database import MSSQLDatabase
 
 
-def create_inserter_objects() -> PandasSQLDataInserter:
-    db_instance = MSSQLDatabaseConnection(
-        settings.MSSQL_SERVER,
-        settings.MSSQL_DATABASE,
-        settings.MSSQL_USERNAME,
-        settings.MSSQL_PASSWORD,
-    )
-    data_inserter = PandasSQLDataInserter(
-        db_instance, max_retries=settings.INSERTER_MAX_RETRIES
-    )
-
-    return data_inserter
+def init_db_instance():
+    return MSSQLDatabase()
 
 
 def fetch_isins() -> list[str]:
-    db_instance = MSSQLDatabaseConnection(
-        settings.MSSQL_SERVER,
-        settings.MSSQL_DATABASE,
-        settings.MSSQL_USERNAME,
-        settings.MSSQL_PASSWORD,
-    )
+    db_instance = init_db_instance()
     query = settings.DB_ISIN_QUERY
-    if db_instance.engine is None:
-        db_instance.connect()
-
-    with db_instance.engine.connect() as connection:
-        result = connection.execute(text(query))
-        isins = [row[0] for row in result]
-
+    isins = db_instance.select_table(query)["isin"].tolist()
     return isins
--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 from client import Engine
 from config import logger, settings
-from database.helper import create_inserter_objects
+from database.helper import init_db_instance
 from transformer import transform
 
 
 def main():
     logger.info("Preparing Database Connection")
-    inserter = create_inserter_objects()
     logger.info("Initializing Scraper Engine")
     engine = Engine(settings.APP_ID, settings.CLIENT_ID, settings.CLIENT_SECRET)
     engine.run()
     logger.info("Transforming Data")
     transformed_dfs = transform(engine)
     logger.info("Preparing Database Inserter")
+    db_instance = init_db_instance()
     for table_name, df in transformed_dfs.items():
         if df is None:
             logger.warning(f"No data to be inserted into {table_name}")
             continue
 
         logger.info(f"\n{df}")
-        df.to_csv(f"{table_name}.csv")
-        inserter.insert(df, table_name)
+        db_instance.insert_table(df, table_name)
 
     logger.info("Application completed successfully")
     return
